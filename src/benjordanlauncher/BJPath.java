f50f54902c23c731f10c8dae95b81528aa55e9d9
Andrew Brown <Andrew@brownfamilymail.co.uk>
2012-08-27 04:20:54 +0100

    Initial Commit

---
+++ src/benjordanlauncher/BJPath.java
@@ -0,0 +1,33 @@
+package benjordanlauncher;
+
+import java.util.prefs.Preferences;
+
+public class BJPath {
+
+    private Preferences prefs;
+    public static final String DEFAULT = "C:\\Games";
+    public static final String ID = "BJPath";
+    public static final String ID2 = "IsPathSet";
+    
+    BJPath(){
+        prefs = Preferences.userRoot().node(this.getClass().getName());
+    }
+    
+    public void setDefaultPath(String path) {
+        prefs.put(ID, path);
+    }
+    
+    public String getDefaultPath() {
+        String defaultPath = prefs.get(ID, DEFAULT);
+        return defaultPath;
+    }
+    
+    public boolean getSetup() {
+        return prefs.getBoolean(ID2, false); 
+    } 
+    
+    public void setSetup(boolean b) {
+        prefs.putBoolean(ID2, b);
+    }
+    
+}
